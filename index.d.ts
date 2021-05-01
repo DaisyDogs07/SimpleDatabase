9644ea570e8afa3829f3139f46da2fbc9657ca8f
DaisyDogs07 <74784459+DaisyDogs07@users.noreply.github.com>
2021-05-01 15:59:01 -0500

    Create index.d.ts

---
+++ index.d.ts
@@ -0,0 +1,24 @@
+declare module 'SimpleDatabase' {
+  import { EventEmitter } from 'events';
+  export class DatabaseOptions {
+    constructor(spaces?: number);
+    public spaces?: number;
+  }
+  export class Database {
+    constructor(location?: string, options?: DatabaseOptions);
+    private spaces?: number;
+    private FilePath?: string;
+    public toJSON(): string;
+    public add(path: string, amount?: number): this;
+    public sub(path: string, amount?: number): this;
+    public set(path: string, value: any): this;
+    public get(path: string): any;
+    public delete(path): boolean;
+    private read(): object;
+    private static DatabaseOptions: DatabaseOptions;
+
+    public on(event: 'change', listener: (path: string, oldData: object, newData: object) => void): this;
+    public once(event: 'change', listener: (path: string, oldData: object, newData: object) => void): this;
+    public emit(event: 'change', listener: (path: string, oldData: object, newData: object) => void): this;
+  }
+}
