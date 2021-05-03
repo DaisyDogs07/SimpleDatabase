50803dd945d94f502b7d1d2505123770560bed0f
DaisyDogs07 <74784459+DaisyDogs07@users.noreply.github.com>
2021-05-03 10:33:24 -0500

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -6,19 +6,20 @@ declare module 'SimpleDatabase' {
   }
   export class Database {
     constructor(location?: string, options?: DatabaseOptions);
-    private spaces?: number;
-    private FilePath?: string;
-    public toJSON(): string;
     public add(path: string, amount?: number): this;
     public sub(path: string, amount?: number): this;
     public set(path: string, value: any): this;
     public get(path: string): any;
     public delete(path: string): boolean;
-    private read(): object;
-    private static DatabaseOptions: DatabaseOptions;
 
     public on(event: 'change', listener: (path: string, oldData: object, newData: object) => void): this;
     public once(event: 'change', listener: (path: string, oldData: object, newData: object) => void): this;
     public emit(event: 'change', listener: (path: string, oldData: object, newData: object) => void): this;
+
+    private read(): object;
+    private static DatabaseOptions: DatabaseOptions;
+    private spaces?: number;
+    private FilePath?: string;
+    private toJSON(): string;
   }
 }
