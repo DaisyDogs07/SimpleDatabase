ee9f50cd3dd0435c430fd78403fd1f7e517df6ce
DaisyDogs07 <>
2021-05-24 16:38:36 +0000

    Added new tests for properties + comments

--- testDatabase.js
+++ testDatabase.js
@@ -1,20 +1,38 @@
 const Database = require('./'),
   fs = require('fs'),
-  database = new Database(__dirname + '/database.json');
+  database = new Database(__dirname + '/database');
 
-if (database.toString() !== '{}')
-  database.clear(); // Get rid of unwanted data
+// Testing filePath
+if (database.filePath !== (__dirname + '/database.json'))
+  throw new TypeError('FilePath property is incorrect');
 
+// Testing spaces
+if (database.spaces !== 2)
+  throw new TypeError('Spaces property is incorrect');
+
+database.clear(); // Get rid of unwanted data
+
+// Setting up tests for add(), sub(), set(), And get()
 database.set('set1', '')
   .set('set2', 1)
   .set('set3', [])
   .set('set4', {})
-  .set('set5', null);
+  .set('set5', null)
+  .set('add', 1)
+  .set('sub', 3);
+
+// Testing add(), And sub()
+database.add('add')
+  .add('add', 2);
+
+database.sub('sub')
+  .sub('sub', 2);
 
 let listener = () => {
   throw new TypeError('Change event fired when not supposed to');
 }
 
+// Testing change event (While also testing set(), add(), And sub())
 database.on('change', listener);
 
 database.set('set1', '')
@@ -24,53 +42,43 @@ database.set('set1', '')
   .set('set5', null)
   .set('set6', undefined);
 
-database.off('change', listener);
-
-database.set('add', 1)
-  .set('sub', 3);
-
-database.add('add')
-  .add('add', 2);
-
-database.sub('sub')
-  .sub('sub', 2);
-
-database.on('change', listener);
-
 database.add('add', 0)
   .sub('sub', 0);
 
 database.off('change', listener);
 
-let expectedValue = '';
 listener = value => {
   if (value !== expectedValue)
     throw new TypeError(`Value didn't match expected value\nExpected: ${expectedValue}\nRecieved: ${value}`);
 }
 
+// Testing get()
+let expectedValue = '';
 listener(database.get('set1'));
+
 expectedValue = 1;
-
 listener(database.get('set2'));
+
 expectedValue = JSON.stringify([]);
-
 listener(JSON.stringify(database.get('set3')));
+
 expectedValue = JSON.stringify({});
-
 listener(JSON.stringify(database.get('set4')));
+
 expectedValue = null;
-
 listener(database.get('set5'));
-expectedValue = undefined;
 
+expectedValue = undefined;
 listener(database.get('set6'));
 
+// Testing read(), And toString()
 expectedValue = `{"set1":"","set2":1,"set3":[],"set4":{},"set5":null,"add":4,"sub":0}`;
 listener(JSON.stringify(database.read()));
 
 expectedValue = JSON.stringify(database.read(), null, 2);
 listener(database.toString());
 
+// Setting up find(), And fildAll()
 database.set('obj', {
   nest1: {
     nest2: {
@@ -96,6 +104,7 @@ listener(JSON.stringify(database.findAll('obj', v => v.nest2 && v.nest2.value2))
 expectedValue = JSON.stringify([database.get('obj.nest1.value1'),database.get('obj.nest1.duplicateOfValue1')]);
 listener(JSON.stringify(database.findAll('obj.nest1', v => v === 'Boo!')));
 
+// Testing setSpaces()
 [
   1,
   '1'
@@ -107,6 +116,7 @@ listener(JSON.stringify(database.findAll('obj.nest1', v => v === 'Boo!')));
   listener(database.spaces);
 });
 
+// Testing moveTo()
 let d = {
   str: database.toString(),
   filePath: database.filePath
@@ -127,6 +137,7 @@ if (!fs.existsSync(d.filePath))
 if (d.str !== database.toString())
   throw new TypeError(`moveTo() failed\nPrevious file contents: ${d.str}\nCurrent file contents: ${database.toString()}`);
 
+// Removing all Databases for cleanup
 fs.unlinkSync(database.filePath);
 fs.unlinkSync(d.filePath);
 
