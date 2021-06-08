a6c812a3ae0112599f0d93f6ce8353dee63534e9
DaisyDogs07 <74784459+DaisyDogs07@users.noreply.github.com>
2021-06-08 11:03:42 -0500

    Add find() and findAll() and also organized types

--- index.d.ts
+++ index.d.ts
@@ -6,6 +6,7 @@ declare module 'SimpleDatabase' {
   }
   export class Database extends EventEmitter {
     constructor(location?: string, options?: DatabaseOptions);
+    //Modifiers
     public add(path: string, amount?: number): this;
     public sub(path: string, amount?: number): this;
     public set(path: string, value: any): this;
@@ -13,21 +14,29 @@ declare module 'SimpleDatabase' {
     public get(path?: string): object;
     public get(path: string): any;
     public delete(path: string): boolean;
-    public has(path: string): boolean;
     public clear(): void;
-    public read(): object;
+    public moveTo(location: string, deleteFile?: boolean): this;
+    public setSpaces(amount?: number): this;
 
+    //Readers
+    public find<V, K>(path: string, fn: (V: any, K: string) => boolean): V;
+    public findAll<V, K>(path: string, fn: (V: any, K: string) => boolean): V[];
+    private read(): object;
+    public has(path: string): boolean;
+    public entries(): [string, any][];
+    public toJSON(): object;
+    public toString(): string;
+    private spaces: number;
+    public history: object[];
+
+    //Events
     public on(event: 'change', listener: (path: string, oldData: object, newData: object) => void): this;
+    public off(event: 'change', listener: (path: string, oldData: object, newData: object) => void): this;
     public once(event: 'change', listener: (path: string, oldData: object, newData: object) => void): this;
     public emit(event: 'change', listener: (path: string, oldData: object, newData: object) => void): boolean;
 
-    public toString(): string;
-    public moveTo(location: string, deleteFile?: boolean): this;
+    //Utils
     public clone(): this;
-    public history: object[];
-    public entries(): [string, any][];
-    public toJSON(): object;
-    public setSpaces(amount?: number): this;
     public static DatabaseOptions: typeof DatabaseOptions;
     public static default: typeof Database;
     public static Database: typeof Database;
